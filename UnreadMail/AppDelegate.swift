28910be2ada221d894589927d4b314a17d316f07
Eder Zechim <eder.zechim@oracle.com>
2015-02-09 21:29:13 -0200

    Initial Commit

---
+++ UnreadMail/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  UnreadMail
+//
+//  Created by Eder Zechim on 2/9/15.
+//  Copyright (c) 2015 Eder Zechim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
