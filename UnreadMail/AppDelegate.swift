fc5a4c223dd9e9e36f028eaeda5053e4775041a3
Eder Zechim <eder.zechim@oracle.com>
2015-02-11 10:52:43 -0200

    Working version with applescript.

--- UnreadMail/AppDelegate.swift
+++ UnreadMail/AppDelegate.swift
@@ -13,17 +13,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var UnreadMainMenu: NSMenu!
 
-    let menubarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-    let menu: NSMenu = NSMenu()
     let menuItem: NSMenuItem = NSMenuItem()
-    
+    let menubarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+
     var timer: NSTimer = NSTimer()
-    var errorInfo: AutoreleasingUnsafeMutablePointer<NSDictionary?>
-    
+
+    var unread: String = "?"
+    let scriptSource: String = "tell application \"Mail\"\nget unread count of inbox\nend tell"
+
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Hide app from dock and tab
         NSApp.setActivationPolicy(NSApplicationActivationPolicy.Accessory)
-        timer = NSTimer.scheduledTimerWithTimeInterval(30, target:self, selector: Selector("checkInbox"), userInfo: nil, repeats: true)
     }
 
     override func awakeFromNib() {
@@ -31,16 +31,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let icon = NSImage(named: "UnreadMailIconSet")
         icon?.setTemplate(true)
         menubarItem.image = icon
+        menubarItem.title = unread
         menubarItem.menu = UnreadMainMenu
-        menubarItem.title = self.getUnreadCount()
+        updateUnreadCount()
+        timer = NSTimer.scheduledTimerWithTimeInterval(30, target:self, selector: Selector("updateUnreadCount"), userInfo: nil, repeats: true)
     }
     
-    func getUnreadCount() -> String {
-        let unread = "?"
-        let scriptSource: String = "tell application \"Mail\"\nget unread count of inbox\nend tell"
+    func updateUnreadCount(){
+        NSLog("getUnreadCount(): " + unread)
         let script: NSAppleScript = NSAppleScript(source: scriptSource)!
-        let retDesc = script.executeAndReturnError(errorInfo)
-        return unread
+        var errorInfo: NSDictionary?
+        var retDesc = script.executeAndReturnError(&errorInfo)
+        if(retDesc?.stringValue != nil){
+            unread = retDesc!.stringValue!
+        }
+        NSLog("getUnreadCount(): " + unread)
+        menubarItem.title = self.unread
     }
     
     @IBAction func QuitClicked(sender: NSMenuItem) {
