ca59245c197a813d23b6fc0c74143b9dd19b9ec8
Eder Zechim <eder.zechim@oracle.com>
2015-02-11 10:02:45 -0200

    v0.1 of applescript implementation.

--- UnreadMail/AppDelegate.swift
+++ UnreadMail/AppDelegate.swift
@@ -12,41 +12,18 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var UnreadMainMenu: NSMenu!
-    @IBOutlet weak var PrefWindow: NSWindow!
-    @IBOutlet weak var InboxList: NSPopUpButton!
-
 
     let menubarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
     let menu: NSMenu = NSMenu()
     let menuItem: NSMenuItem = NSMenuItem()
     
     var timer: NSTimer = NSTimer()
-    
-    var inboxFile: String = ""
-    let inboxPrefix: String = NSHomeDirectory() + "/Library/Mail/V2/"
-    let inboxSuffix: String = "/INBOX.mbox/Info.plist"
-    let pfile = NSHomeDirectory() + "/.unreadmail.inbox"
+    var errorInfo: AutoreleasingUnsafeMutablePointer<NSDictionary?>
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Hide app from dock and tab
         NSApp.setActivationPolicy(NSApplicationActivationPolicy.Accessory)
         timer = NSTimer.scheduledTimerWithTimeInterval(30, target:self, selector: Selector("checkInbox"), userInfo: nil, repeats: true)
-        
-        self.getInboxFile()
-        
-        if ( self.inboxFile == "" ) {
-            InboxList.addItemWithTitle("choose your inbox path")
-        }
-        if let accountsDict = NSDictionary(contentsOfFile: inboxPrefix + "MailData/Accounts.plist") {
-            let accounts = accountsDict.objectForKey("MailAccounts") as NSArray
-            for mailboxDict in accounts {
-                var mailboxpath = mailboxDict.objectForKey("AccountPath") as String
-                if ( mailboxpath != "~/Library/Mail/V2/Mailboxes" ) {
-                    NSLog("Found mailbox: " + mailboxpath)
-                    InboxList.addItemWithTitle(mailboxpath)
-                }
-            }
-        }
     }
 
     override func awakeFromNib() {
@@ -55,52 +32,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         icon?.setTemplate(true)
         menubarItem.image = icon
         menubarItem.menu = UnreadMainMenu
-        menubarItem.title = "?"
-        self.checkInbox()
+        menubarItem.title = self.getUnreadCount()
     }
     
-    func checkInbox(){
-        NSLog("Trying to read " + inboxFile)
-        if let inbox = NSMutableDictionary(contentsOfFile: inboxFile) {
-            if let unread = inbox.objectForKey("IMAPMailboxUnseenCount") as? Int {
-                menubarItem.title = String(unread)
-            }
-        } else {
-            menubarItem.title = "?"
-        }
-    }
-    
-    func getInboxFile(){
-        NSLog("getInboxFile - start: " + self.inboxFile)
-        let fileManager = NSFileManager.defaultManager()
-        if ((fileManager.fileExistsAtPath(pfile))) {
-            NSLog("file exists: " + pfile)
-            self.inboxFile = String(contentsOfFile: pfile, encoding: NSUTF8StringEncoding, error: nil)!
-        }
-        NSLog("getInboxFile - end: " + self.inboxFile)
-    }
-    
-    func setInboxFile(){
-        NSLog("setInboxFile - start: " + self.inboxFile)
-        let fileManager = NSFileManager.defaultManager()
-        self.inboxFile.writeToFile(pfile, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
-        NSLog("setInboxFile - end: " + self.inboxFile)
-    }
-    
-    @IBAction func PrefClicked(sender: NSMenuItem) {
-        self.PrefWindow!.orderFront(self)
-    }
-    
-    @IBAction func CloseClicked(sender: NSButton) {
-        self.PrefWindow!.orderOut(self)
-    }
-    
-    @IBAction func InboxSelection(sender: NSPopUpButton) {
-        self.inboxFile = InboxList.titleOfSelectedItem! + inboxSuffix
-        self.inboxFile = self.inboxFile.stringByReplacingOccurrencesOfString("~", withString: NSHomeDirectory())
-        setInboxFile()
-        NSLog("inboxFile set to = " + inboxFile)
-        
+    func getUnreadCount() -> String {
+        let unread = "?"
+        let scriptSource: String = "tell application \"Mail\"\nget unread count of inbox\nend tell"
+        let script: NSAppleScript = NSAppleScript(source: scriptSource)!
+        let retDesc = script.executeAndReturnError(errorInfo)
+        return unread
     }
     
     @IBAction func QuitClicked(sender: NSMenuItem) {
