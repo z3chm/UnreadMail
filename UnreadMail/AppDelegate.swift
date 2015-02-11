228a282790d461a4e0ced0f11094ce288ee916f4
Eder Zechim <eder.zechim@oracle.com>
2015-02-10 22:40:36 -0200

    Add preferences, save inbox selection.

--- UnreadMail/AppDelegate.swift
+++ UnreadMail/AppDelegate.swift
@@ -11,23 +11,29 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var UnreadMainMenu: NSMenu!
+    @IBOutlet weak var PrefWindow: NSWindow!
+    @IBOutlet weak var InboxList: NSPopUpButton!
 
-    var menubarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-    var menu: NSMenu = NSMenu()
-    var menuItem: NSMenuItem = NSMenuItem()
+
+    let menubarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let menu: NSMenu = NSMenu()
+    let menuItem: NSMenuItem = NSMenuItem()
     
     var timer: NSTimer = NSTimer()
     
     var unreadMsgs: Int = -1
-    var inboxFile = "/Users/zechim/Library/Mail/V2/IMAP-eder.zechim@oracle.com@stbeehive.oracle.com/INBOX.mbox/Info.plist"
-
+    var inboxFile: String = ""
+    let inboxPrefix: String = NSHomeDirectory() + "/Library/Mail/V2/"
+    let inboxSuffix: String = "/INBOX.mbox/Info.plist"
+    let pfile = NSHomeDirectory() + "/.unreadmail.inbox"
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Hide app from dock and tab
         NSApp.setActivationPolicy(NSApplicationActivationPolicy.Accessory)
         
         timer = NSTimer.scheduledTimerWithTimeInterval(10, target:self, selector: Selector("checkInbox"), userInfo: nil, repeats: true)
+        
     }
 
     override func awakeFromNib() {
@@ -37,18 +43,67 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menubarItem.image = icon
         menubarItem.menu = UnreadMainMenu
         menubarItem.title = "?"
-        
 
+        self.getInboxFile()
+        
+        if ( self.inboxFile == "" ) {
+            InboxList.addItemWithTitle("choose your inbox path")
+        }
+        if let accountsDict = NSDictionary(contentsOfFile: inboxPrefix + "MailData/Accounts.plist") {
+            let accounts = accountsDict.objectForKey("MailAccounts") as NSArray
+            for mailboxDict in accounts {
+                var mailboxpath = mailboxDict.objectForKey("AccountPath") as String
+                if ( mailboxpath != "~/Library/Mail/V2/Mailboxes" ) {
+                    NSLog("Found mailbox: " + mailboxpath)
+                    InboxList.addItemWithTitle(mailboxpath)
+                }
+            }
+        }
+        
         self.checkInbox()
     }
     
     func checkInbox(){
+        NSLog("Trying to read " + inboxFile)
         if let inbox = NSMutableDictionary(contentsOfFile: inboxFile) {
             unreadMsgs = inbox.objectForKey("IMAPMailboxUnseenCount") as Int
             menubarItem.title = String(unreadMsgs)
         } else {
             menubarItem.title = "?"
         }
+    }
+    
+    func getInboxFile(){
+        NSLog("getInboxFile - start: " + self.inboxFile)
+        let fileManager = NSFileManager.defaultManager()
+        if ((fileManager.fileExistsAtPath(pfile))) {
+            NSLog("file exists: " + pfile)
+            self.inboxFile = String(contentsOfFile: pfile, encoding: NSUTF8StringEncoding, error: nil)!
+        }
+        NSLog("getInboxFile - end: " + self.inboxFile)
+    }
+    
+    func setInboxFile(){
+        NSLog("setInboxFile - start: " + self.inboxFile)
+        let fileManager = NSFileManager.defaultManager()
+        self.inboxFile.writeToFile(pfile, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
+        NSLog("setInboxFile - end: " + self.inboxFile)
+    }
+    
+    @IBAction func PrefClicked(sender: NSMenuItem) {
+        self.PrefWindow!.orderFrontRegardless()
+    }
+    
+    @IBAction func CloseClicked(sender: NSButton) {
+        self.PrefWindow!.orderOut(self)
+    }
+    
+    @IBAction func InboxSelection(sender: NSPopUpButton) {
+        self.inboxFile = InboxList.titleOfSelectedItem! + inboxSuffix
+        self.inboxFile = self.inboxFile.stringByReplacingOccurrencesOfString("~", withString: NSHomeDirectory())
+        setInboxFile()
+        NSLog("inboxFile set to = " + inboxFile)
+        
     }
     
     @IBAction func QuitClicked(sender: NSMenuItem) {
